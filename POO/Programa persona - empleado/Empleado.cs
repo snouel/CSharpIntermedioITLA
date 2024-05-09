faedbfce4d1e1f50ed641decb716036e59fb555d
Steven Nouel <s.nouel@unapec.edu.do>
2024-05-08 21:25:31 -0400

    Programa persona-empleado

---
+++ POO/Programa persona - empleado/Empleado.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Programa_persona___empleado
+{
+    class Empleado : Persona
+    {
+        public double Sueldo {get ; set;}
+
+        public Empleado(string nombre, int edad, double sueldo): base (nombre, edad)
+        {
+            Sueldo = sueldo;
+        }
+
+    }
+
+    class EmpleadoPrinter
+    {
+        public void Imprimir(Empleado empleado)
+        {
+            Console.WriteLine($"Nombre: {empleado.Nombre}, Edad: {empleado.Edad}, Sueldo: {empleado.Sueldo}");
+        }
+    }
+}
---
+++ POO/Programa persona - empleado/Program.cs
@@ -0,0 +1,16 @@
+﻿using Programa_persona___empleado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona("Manuel", 40);
+        PersonaPrinter personaPrinter = new PersonaPrinter();
+        personaPrinter.Imprimir(persona);
+
+        Empleado empleado = new Empleado("Pedro", 20, 50000);
+        EmpleadoPrinter empleadoPrinter = new EmpleadoPrinter();
+        empleadoPrinter.Imprimir(empleado); 
+
+    }
+}
