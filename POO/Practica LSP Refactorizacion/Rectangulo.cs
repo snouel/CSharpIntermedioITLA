3598342b315526defb46cb936ab22f49eea19a5b
Steven Nouel <89994716+snouelhub@users.noreply.github.com>
2024-05-17 23:10:42 -0400

    Practica LSP

---
+++ POO/Practica LSP Refactorizacion/Rectangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_LSP_Refactorizacion
+{
+    public class Rectangulo : IFiguraGeometrica
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
